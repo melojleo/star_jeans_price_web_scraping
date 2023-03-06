0cbaddb745ae9ac995dfe6d0a8902f09cb9b38e2
melojleo <melojleo@hotmail.com>
2023-03-06 11:56:40 -0300

    commit

--- project_web_scraping.py
+++ project_web_scraping.py
@@ -1,5 +1,9 @@
 from bs4 import BeautifulSoup
 import requests 
+import pandas as pd
+import numpy as np
+from datetime import datetime
+
 
 
 url = 'https://www2.hm.com/en_gb/men/shop-by-product/jeans.html'
@@ -9,18 +13,42 @@ page = requests.get(url, headers=headers)
 
 soup = BeautifulSoup(page.text, 'html.parser')
 
+#Total item and pages
+total_item = soup.find_all('h2', class_='load-more-heading')[0].get('data-total')
+total_item = int(total_item)
+page_number = np.round(int(total_item)/36)
+
+#Final Url
+url02 = url + '?page-size=' + str(int(page_number*36))
+page = requests.get(url02, headers=headers)
+soup = BeautifulSoup(page.text, 'html.parser')
+
+#list of all products
 products = soup.find('ul', class_='products-listing small')
 
-product_list = products.find_all('article', class_='hm-product-item')
-
 # Product id:
+product_list = products.find_all('article', class_='hm-product-item')
 product_id = [p.get('data-articlecode') for p in product_list]
 
 #produt category:
 product_category = [p.get('data-category') for p in product_list]
 
 #produt name:
-#product_name = [p.get('data-category') for p in product_list]
 product_list = products.find_all('a', class_='link')
 product_name = [p.get_text() for p in product_list]
-print(product_category)
+
+#price:
+product_list = products.find_all('span', class_='price regular')
+product_price = [p.get_text() for p in product_list]
+
+#color
+
+#composition
+
+data_raw = pd.DataFrame([product_id, product_category, product_name, product_price]).T
+data_raw.columns = ['product_id', 'product_category', 'product_name', 'product_price']
+ 
+#srapy datetime
+data_raw['scrapy_datetime'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+ 
+print(data_raw)
